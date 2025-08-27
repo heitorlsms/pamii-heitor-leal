4aab1c2a28d5aa3ec9df62ca0bb96782521da8ba
Heitor Leal <heitorlsms@gmail.com>
2025-08-27 08:40:00 -0300

    atividade typescript básico

---
+++ 2-01/declaracoes.ts
@@ -0,0 +1,30 @@
+//Declaralções de variáveis
+let nome: string ='João';
+let idade: number = 25;
+let adesivo: boolean = true;
+
+//Arrays
+let numneros: number[] = [1, 2, 3, 4, 5];
+let nomes: string[] = ['Ana', 'Bruno', 'Carlos'];
+let misto: (string | number)[] = ['Ana', 25, 'Carlos', 30];
+let msito2: Array<string | number> = ['Ana', 25, 'Carlos', 30];
+
+//Tuplas
+let pessoa: [string, number] = ['Maria', 30];
+
+//Union Types
+let id: number | string = 123;
+id = 'ABC123';
+
+//Interfaces - são usadas para definir estrutura de objetos
+interface Usuario {
+    nome:string;
+    idade:number;
+    email?: string; //Opcional
+}
+
+//Utilizar elas fica assim:
+let novo_usuario: Usuario = {
+    nome: 'Amanda',
+    idade: 18
+};
